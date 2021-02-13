a3686b6ed525f265200e752e1874ea164cccaae7
Ali Pishvaee <alipishvaee2@Gmail.com>
2021-02-13 23:25:49 +0330

    Add RxSwift and RxCocoa to project

---
+++ Pods/Target Support Files/Pods-Number'sPersianSpellOut/Pods-Number'sPersianSpellOut-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Number_sPersianSpellOut : NSObject
+@end
+@implementation PodsDummy_Pods_Number_sPersianSpellOut
+@end
---
+++ Pods/Target Support Files/Pods-Number'sPersianSpellOutTests/Pods-Number'sPersianSpellOutTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Number_sPersianSpellOutTests : NSObject
+@end
+@implementation PodsDummy_Pods_Number_sPersianSpellOutTests
+@end
